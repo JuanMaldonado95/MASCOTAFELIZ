cca2e11de81bcfee6a66ad5130ad1bf4d2e04ebf
JuanMaldonado95 <112447336+JuanMaldonado95@users.noreply.github.com>
2022-09-02 22:12:13 -0500

    Add files via upload

---
+++ MascotaFeliz.App.Dominio/Entidades/Dueno.cs
@@ -0,0 +1,8 @@
+using System;
+namespace MascotaFeliz.App.Dominio{
+    public class Dueno:Persona // Veterinario.Persona denota herencia
+    {
+        public string correo {get;set;}
+    }
+
+}
---
+++ MascotaFeliz.App.Dominio/Entidades/Veterinario.cs
@@ -0,0 +1,8 @@
+using System;
+namespace MascotaFeliz.App.Dominio{
+    public class Veterinario:Persona // Veterinario.Persona denota herencia
+    {
+        public string TarjetaProfesional {get;set;}
+    }
+
+}
